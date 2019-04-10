1da9ca944f2343dda124ad046b85440533542239
fanjinchang <894020798@qq.com>
2019-04-10 20:04:15 +0800

    hhhh

--- test/sketch_test_fft.js
+++ test/sketch_test_fft.js
@@ -1,9 +1,10 @@
 function preload(){
   //sound = loadSound('test.mp3');//ok需开启www服务，用浏览器打开网页http://localhost/bideyuanli_test.html，而不是打开文件
-  sound = loadSound('./test/xiaoyao1.mp3');//加载不了
+  sound = loadSound('xiaoyao1.mp3');//ok
+  // sound = loadSound('erquanyingyue.mp3'); //error
+  // sound = loadSound('erquan_part_d.mp3'); //error
 }
 
- 
 
 function setup(){
   var cnv = createCanvas(100,100);
@@ -12,18 +13,24 @@ function setup(){
   sound.amp(0.2);
 }
 
+var Flag=0;
+
 function draw(){
+	sleep(2000);
   background(0);
 
   var spectrum = fft.analyze();
   noStroke();
   fill(0,255,0); // spectrum is green
-  console.log(spectrum.length)
+  
   for (var i = 0; i< spectrum.length; i++){
     var x = map(i, 0, spectrum.length, 0, width);
     var h = -height + map(spectrum[i], 0, 255, height, 0);
     //rect(x, height, width / spectrum.length, h )
 	rect(x, 0, width / spectrum.length, h )
+	if(Flag==0){
+		console.log(spectrum[i])
+	}
   }
 
   var waveform = fft.waveform();
@@ -39,6 +46,8 @@ function draw(){
   endShape();
 
   text('click to play/pause', 4, 10);
+  
+  Flag=Flag+1;
 }
 
 // fade sound if mouse is over canvas
@@ -47,5 +56,12 @@ function togglePlay() {
     sound.pause();
   } else {
     sound.loop();
+  }
+}
+
+function sleep(delay) {
+  var start = (new Date()).getTime();
+  while ((new Date()).getTime() - start < delay) {
+    continue;
   }
 }
